f68a341cdb9b2b74d93cf05f4765e769aa413943
Samuel BERTHE <contact@samuel-berthe.fr>
2016-12-14 13:58:04 +0100

    fix

--- celery_prometheus_exporter.py
+++ celery_prometheus_exporter.py
@@ -31,9 +31,9 @@ def check_worker_general_failure(worker_pool_size):
             UPTIME = prometheus_client.Gauge('celery_last_failure_time', 'Timestamp of the last failure, to compute uptime')
         UPTIME.set(int(round(time.time() * 1000)))
 
-class MonitorThread(threading.Thread):
+class TaskMonitoringThread(threading.Thread):
     """
-    MonitorThread is the thread that will collect the data that is later
+    TaskMonitoringThread is the thread that will collect the data that is later
     exposed from Celery using its eventing system.
     """
 
@@ -66,7 +66,7 @@ class MonitorThread(threading.Thread):
             elif event['type'].find("worker") is not -1:
                 worker_pool_size = len([w for w in self._state.workers.values() if w.alive])
                 WORKERS.set(worker_pool_size)
-                check_worker_general_failure(worker_pool_size)
+#                check_worker_general_failure(worker_pool_size)
             else:
                 self.log.info("Unknown event type: " + event['type'])
 
@@ -83,23 +83,20 @@ class MonitorThread(threading.Thread):
                 self.log.error("Queue connection failed", e)
                 time.sleep(5)
 
+class WorkerMonitoringThread(threading.Thread):
+    def __init__(self, *args, app=None, **kwargs):
+        self._app = app
+        self.log = logging.getLogger('monitor')
+        super().__init__(*args, **kwargs)
 
-# override the prometheus_client handler to ping workers
-# this is needed because if every producers, workers and celerybeat die, _process_event could never be called
-class CustomMetricsHandler(prometheus_client.MetricsHandler):
-    def __init__(self, *args, **kwargs):
-        self._app = celery.Celery(broker=DEFAULT_BROKER)
-        super(CustomMetricsHandler, self).__init__(*args, **kwargs)
+    def run(self):
+        self._monitor()
 
-    def do_GET(self):
-        super(CustomMetricsHandler, self).do_GET()
-        # pinging workers can take some time
-        # so we won't do it at each scrape
-        # we ping workers after metric collection because prometheus store the timestamp of the request beginning
-        if random.randint(0, 10) is 0:
-            workers = self._app.control.ping(timeout=1)
+    def _monitor(self):
+        while True:
+            workers = self._app.control.ping(timeout=5)
             WORKERS.set(len(workers))
-            check_worker_general_failure(len(workers))
+            time.sleep(5)
 
 def start_httpd(addr):
     """
@@ -108,17 +105,7 @@ def start_httpd(addr):
     """
     host, port = addr.split(':')
     logging.info('Starting HTTPD on {}:{}'.format(host, port))
-
-    class PrometheusMetricsServer(threading.Thread):
-        def run(self):
-            from http.server import HTTPServer
-            httpd = HTTPServer((host, int(port)), CustomMetricsHandler)
-            httpd.serve_forever()
-    t = PrometheusMetricsServer()
-    t.daemon = True
-    t.start()
-#    prometheus_client.start_http_server(int(port), host)
-
+    prometheus_client.start_http_server(int(port), host)
 
 def shutdown(signum, frame):
     """
@@ -156,11 +143,16 @@ def main():
 
     global DEFAULT_BROKER
     DEFAULT_BROKER = opts.broker
-    t = MonitorThread(app=celery.Celery(broker=opts.broker))
+    celery_app = celery.Celery(broker=opts.broker)
+    t = TaskMonitoringThread(app=celery_app)
+    w = WorkerMonitoringThread(app=celery_app)
     t.daemon = True
+    w.daemon = True
     t.start()
+    w.start()
     start_httpd(opts.addr)
     t.join()
+    w.join()
 
 
 if __name__ == '__main__':
